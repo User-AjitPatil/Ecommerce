2c87946a843d70fe76ff52695dbbd77f23045259
User-AjitPatil <ajitpatil7805@gmail.com>
2024-11-12 20:58:02 +0530

    initial commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div>
+      <div className='spinner justify-center items-center align-middle mt-72 ml-96 -mr-96'></div>
+    </div>
+  )
+}
+
+export default Spinner
